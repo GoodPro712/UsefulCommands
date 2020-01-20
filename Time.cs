4a9f29cb0b1ade69cda69d5510b799a237dbcfb5
Stevie <xxlennygamerxx@gmail.com>
2020-01-19 17:34:55 -0800

    Started merging into Useful Commands
    * DisplayName update
    
    * credit
    
    * version
    
    * Description
    
    * lol
    
    * Update GameModeCmd.cs
    
    * !
    
    * update to match description.txt
    
    * Update GameModeCmd.cs
    
    * file transfer
    
    * tbh no point in actually having it
    
    * Update Gamemode.cs
    
    * Update Gamemode.cs
    
    * Day and Night
    
    * Update GameModeCmd.cs
    
    * Update README.md
    
    * fixed author
    
    * removed (convicted tomatophile)

---
+++ Time.cs
@@ -0,0 +1,41 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+
+namespace GameModeCmd
+{
+    public class Time : ModCommand
+    {
+        public override CommandType Type => CommandType.Chat | CommandType.World;
+        public override string Command => "time";
+        public override string Usage => "/time <day/night>";
+        public override string Description => "Sets the time.";
+
+        public override void Action(CommandCaller caller, string input, string[] args)
+        {
+            if (args.Length > 0)
+            {
+                string name = string.Join(" ", args);
+                if (name == "day")
+                {
+                    Main.dayTime = true;
+                    Main.time = 27000.0;
+                    Main.NewText("SUCCESS: Time set to day!");
+                }
+                else if (name == "night")
+                {
+                    Main.dayTime = false;
+                    Main.time = 0;
+                    Main.NewText("SUCCESS: Time set to night!");
+                }
+                else
+                {
+                    Main.NewText("ERROR: You can only set the time to DAY or NIGHT!");
+                }
+            }
+            else
+            {
+                Main.NewText("ERROR: You have not specified a time!");
+            }
+        }
+    }
+}
